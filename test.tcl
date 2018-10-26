b83470c4d0fc002f746d93814f4a54588b73f353
zhaoxin-pc <RobinHuo@zhaoxin.com>
2018-10-26 16:27:50 +0800

    no care

--- test.tcl
+++ test.tcl
@@ -4,7 +4,15 @@ set b "world!"
 puts $a 
 puts $b
 
-button .hello \
-	-text Hello \
-	-command {puts stdout "$a$b"}
-	pack .hello -padx 20 -pady 10
+#button .hello \
+#	-text Hello \
+#	-command {puts stdout "$a$b"}
+#	pack .hello -padx 20 -pady 10
+
+set zzy z
+append zzy a b c d
+puts $zzy
+
+set num 20
+set num_ [format "%2\$+-020d" $num 50 0.3 labe]
+puts $num_
